2a8d3a0cd0473c0d85782f35c860565fadd554f8
Dennis Fischer <dennis.fischer@live.com>
2017-03-18 11:34:59 +0100

    Fixed most bugs

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem pdflatex -synctex=1 -shell-escape -interaction=nonstopmode -output-directory=build thesis.tex
-rem biber build/thesis.bcf
-makeglossaries -d build thesis
-rem pdflatex -synctex=1 -shell-escape -interaction=nonstopmode -output-directory=build thesis.tex
