3e1e9bab27b4de1b78020c33e324a5c2f81df20f
Dennis Fischer <dennis.fischer@live.com>
2017-03-18 09:51:27 +0100

    Added missing files

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
 lualatex -synctex=1 -shell-escape -interaction=nonstopmode -output-directory=build thesis.tex
 biber build/thesis.bcf
-lualatex -synctex=1 -shell-escape -interaction=nonstopmode -output-directory=build thesis.tex
 makeglossaries -d build thesis
 lualatex -synctex=1 -shell-escape -interaction=nonstopmode -output-directory=build thesis.tex
